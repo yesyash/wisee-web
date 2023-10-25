7cbc3e97bb8ef65e5276e591bcabcdbceff6eda3
Yash Raj <yashraj0207@outlook.com>
2023-10-25 23:43:17 +0530

    Setup storybook with support for tailwind, next/font & path aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
 
+import { inter } from "@/fonts";
 import { cn } from "@/utils/classname";
 import "@/styles/globals.css";
-
-const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-inter" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
