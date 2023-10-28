af1c3a3e29c47998dd8b9832f6c936b398fafdb2
Yash Raj <yashraj0207@outlook.com>
2023-10-29 01:56:38 +0530

    Fix styles for outline, ghost & link variants in button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -8,24 +8,50 @@ import { Loader2Icon } from "lucide-react"
 import { cn } from "@/utils/classname"
 
 const buttonVariants = cva(
-    "inline-flex items-center gap-2 justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    [
+        "flex items-center gap-2 justify-center rounded-md text-sm font-medium transition text-gray-900",
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring",
+        "focus-visible:ring-offset-2 focus-visible:ring-blue-400",
+        "disabled:pointer-events-none",
+        "[&>svg]:w-4 [&>svg]:h-4"
+    ],
     {
         variants: {
             variant: {
-                primary: "bg-blue-600 text-stone-50 hover:bg-blue-700 active:bg-blue-800",
-                destructive:
-                    "bg-red-600 text-stone-50 hover:bg-red-700 active:bg-red-800",
-                outline:
-                    "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-                secondary:
-                    "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-                ghost: "hover:bg-accent hover:text-accent-foreground",
-                link: "text-primary underline-offset-4 hover:underline",
+                primary: [
+                    "bg-blue-600 text-gray-50",
+                    "hover:bg-blue-700 active:bg-blue-800 ",
+                    "disabled:bg-gray-300 disabled:text-gray-500 data-[disabled]:bg-gray-300 data-[disabled]:text-gray-500"
+                ],
+                secondary: [
+                    "bg-blue-50 text-blue-600",
+                    "hover:bg-blue-100 active:bg-blue-200 active:text-blue-800",
+                    "disabled:bg-gray-100 disabled:text-gray-600 data-[disabled]:bg-gray-100 data-[disabled]:text-gray-600"
+                ],
+                outline: [
+                    "border border-gray-300 text-gray-900",
+                    "hover:bg-gray-100 active:bg-gray-200",
+                    "disabled:bg-gray-100 disabled:text-gray-500 data-[disabled]:bg-gray-100 data-[disabled]:text-gray-500"
+                ],
+                danger: [
+                    "bg-red-500 text-gray-50",
+                    "hover:bg-red-600 active:bg-red-700 focus-visible:ring-red-400",
+                    "disabled:opacity-50 data-[disabled]:opacity-50"
+                ],
+                ghost: [
+                    "text-gray-900",
+                    "hover:bg-gray-100 active:bg-gray-200",
+                    "disabled:opacity-80 data-[disabled]:opacity-80"
+                ],
+                link: [
+                    "hover:underline underline-offset-4",
+                    "disabled:opacity-80 data-[disabled]:opacity-80"
+                ],
             },
             size: {
                 default: "h-10 px-4 py-2",
-                sm: "h-9 rounded-md px-3",
-                lg: "h-11 rounded-md px-8",
+                sm: "h-9 rounded-md px-3 font-sm",
+                lg: "h-11 rounded-md px-8 text-base [&>svg]:w-5 [&>svg]:h-5",
                 icon: "h-10 w-10",
             },
         },
@@ -55,11 +81,11 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             >
                 {asChild ? (
                     React.isValidElement(children) ? (
-                        React.cloneElement(children, undefined, loading && <Loader2Icon />, children.props.children)
+                        React.cloneElement(children, undefined, loading && <Loader2Icon className="animate-spin" />, children.props.children)
                     ) : null
                 ) : (
                     <>
-                        {loading && <Loader2Icon />}
+                        {loading && <Loader2Icon className="animate-spin" />}
                         {children}
                     </>
                 )}
